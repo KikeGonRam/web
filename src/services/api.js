5ad8e57634ca45ca741faf9286bbc36efd0b2ddf
KikeGonRam <kikeramirez160418@email.com>
2025-04-11 17:05:52 -0600

    API integrada

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:5000',
+  baseURL: 'https://3.130.121.198/',
 });
 
 // Interceptor para añadir el token a las peticiones
